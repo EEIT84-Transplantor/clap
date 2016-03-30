356abbafefbe57be640c0784f82b3f7dfa69639a
caca <jlin218@uw.edu>
2016-03-30 19:41:38 +0800

    service edit jdbc localrepo

--- Clap/src/main/java/service/MemberService.java
+++ Clap/src/main/java/service/MemberService.java
@@ -1,26 +1,51 @@
 package service;
 
-public class MemberService {
+import java.util.Arrays;
+import java.util.function.IntPredicate;
 
+import dao.MemberDAO;
+import dao.MemberJdbcDAO;
+import model.MemberVO;
+
+public class MemberService {
+	private MemberDAO dao;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MemberService ms= new MemberService();
+		System.out.println(ms.login("andrew@gmail.com", "andrew"));
 	}
-//	
-//	public memberVO login (String email, String password){
+	
+	public MemberService(){
+		dao = new MemberJdbcDAO();
+	}
+	
+	public MemberVO login (String email, String password){
+		MemberVO result=null;
+		MemberVO member = dao.selectByEmail(email);
+		if (member!= null){
+			byte[] temp = password.getBytes();
+			byte[] memberPassword = member.getPassword();
+			if (Arrays.equals(temp, memberPassword)){
+				result = member;
+			}
+		}
+		return result;
+	}
+//	public MemberVO signUp (String email, String password) {
 //		
 //	}
-//	public memberVO signUp (String email, String password) {
-//		
-//	}
-//	public boolean changePassword (String email, String password) {
-//		
-//	}
+
+	public boolean changePassword (String email, String password) {
+		return dao.update(email, password.getBytes());
+	}
+
 //	public boolean sendComfirmEmail (String email){
 //		
 //	}
-//	public memberVO findByEmail (Stirng email){
-//		
-//	}
+
+	public MemberVO findByEmail (String email){
+		MemberVO member = dao.selectByEmail(email);
+		return member;
+	}
 
 }
