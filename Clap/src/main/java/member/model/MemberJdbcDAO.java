4af89614cb50ec6a7db23b3f4d3fbb2364e31697
au4au4g4 <au4au4g4@gmail.com>
2016-03-31 01:43:32 +0800

    修改 MemberJdbcDAO

--- Clap/src/main/java/member/model/MemberJdbcDAO.java
+++ Clap/src/main/java/member/model/MemberJdbcDAO.java
@@ -1,7 +1,6 @@
 package member.model;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,57 +9,61 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import member.model.MemberVO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MemberJdbcDAO implements MemberDAO {
-	private DataSource dataSource;
-	private Connection conn =null;
-	
 
+	// 測試程式
 	public static void main(String[] args) {
-		MemberDAO mdao= new MemberJdbcDAO();
-//		System.out.println(mdao.selectAll());
-		System.out.println(mdao.delete("lala42@gmail.com"));
-		
-		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		MemberDAO mdao = (MemberDAO) context.getBean("memberDAO");
+		System.out.println(mdao.selectAll());
 	}
-	public MemberJdbcDAO(){
-//		try {
-//			System.out.println(dataSource);
-//			conn=dataSource.getConnection();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
+
+	private DataSource dataSource;
+	private Connection conn = null;
+	private static final String SELECT_ALL = "select * from Member";
+	private static final String SELECT_BY_EMAIL = "select * from Member where email=?";
+	private static final String INSERT = "insert into Member(email,password) values(?,?)";
+	private static final String UPDATE = "update Member set password=? where email=?";
+	private static final String DELETE = "delete from Member where email=?";
+
+	public MemberJdbcDAO() {
+//		 try {
+//		 System.out.println(dataSource);
+//		 conn=dataSource.getConnection();
+//		 } catch (SQLException e) {
+//		 e.printStackTrace();
+//		 }
 	}
+
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
-	private static final String SELECT_ALL="select * from Member";
-	/* (non-Javadoc)
-	 * @see dao.MemberDAO#selectAll()
-	 */
+
 	@Override
-	public List<MemberVO> selectAll(){
+	public List<MemberVO> selectAll() {
 		List<MemberVO> result = null;
-		
-		try {
-			conn=dataSource.getConnection();
+
+		try {		
+			conn = dataSource.getConnection();
 			PreparedStatement ps = conn.prepareStatement(SELECT_ALL);
-			ResultSet rs;	
-			rs= ps.executeQuery();
-			while(rs.next()){
-				if(result == null){
+			ResultSet rs;
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				if (result == null) {
 					result = new ArrayList<MemberVO>();
 				}
 				MemberVO mvo = new MemberVO();
 				mvo.setEmail(rs.getString("email"));
 				mvo.setPassword(rs.getBytes("password"));
-				result.add(mvo);	
-			}	
+				result.add(mvo);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if(conn!=null){
+		} finally {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
@@ -70,141 +73,120 @@ public class MemberJdbcDAO implements MemberDAO {
 		}
 		return result;
 	}
-	
-	private static final String SELECT_BY_EMAIL="select * from Member where email=?";
-	
-	/* (non-Javadoc)
-	 * @see dao.MemberDAO#selectByEmail(java.lang.String)
-	 */
+
 	@Override
-	public MemberVO selectByEmail(String email){
+	public MemberVO selectByEmail(String email) {
 		MemberVO result = null;
 		try {
 			PreparedStatement ps = conn.prepareStatement(SELECT_BY_EMAIL);
-			ResultSet rs;	
+			ResultSet rs;
 			ps.setString(1, email);
-			rs= ps.executeQuery();
-			if(rs.next()){
+			rs = ps.executeQuery();
+			if (rs.next()) {
 				result = new MemberVO();
 				result.setEmail(rs.getString("email"));
 				result.setPassword(rs.getBytes("password"));
-			}	
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if(conn!=null){
+		} finally {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 		return result;
 	}
-	
-	private static final String INSERT="insert into Member(email,password) values(?,?)";
-	
-	/* (non-Javadoc)
-	 * @see dao.MemberDAO#insert(java.lang.String, byte[])
-	 */
+
 	@Override
-	public MemberVO insert(String email, byte[]password){
+	public MemberVO insert(String email, byte[] password) {
 		MemberVO result = null;
-		
+
 		try {
 			PreparedStatement ps = conn.prepareStatement(INSERT);
 			ps.setString(1, email);
 			ps.setString(2, password.toString());
-			
+
 			int returnedValue = ps.executeUpdate();
-			if(returnedValue==1){
+			if (returnedValue == 1) {
 				result = new MemberVO();
 				result.setEmail(email);
-				result.setPassword(password);;
+				result.setPassword(password);
+				;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if(conn!=null){
+		} finally {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 		return result;
 	}
-	
-	
-	
-	private static final String UPDATE="update Member set password=? where email=?";
-	
-	/* (non-Javadoc)
-	 * @see dao.MemberDAO#update(java.lang.String, byte[])
-	 */
+
 	@Override
-	public boolean update(String email, byte[]password){
+	public boolean update(String email, byte[] password) {
 		boolean result = false;
-		
+
 		try {
 			PreparedStatement ps = conn.prepareStatement(UPDATE);
-			System.out.println(email+password);
+			System.out.println(email + password);
 			ps.setString(1, password.toString());
 			ps.setString(2, email);
 			int returnedValue = ps.executeUpdate();
-			if(returnedValue==1){
-				result=true;
+			if (returnedValue == 1) {
+				result = true;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if(conn!=null){
+		} finally {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 		System.out.println(result);
 		return result;
 	}
-	
-	private static final String DELETE="delete from Member where email=?";
-	
-	/* (non-Javadoc)
-	 * @see dao.MemberDAO#delete(java.lang.String)
-	 */
+
 	@Override
-	public boolean delete(String email){
+	public boolean delete(String email) {
 		boolean result = false;
-		
+
 		try {
 			PreparedStatement ps = conn.prepareStatement(DELETE);
-	
+
 			ps.setString(1, email);
 			int returnedValue = ps.executeUpdate();
-			if(returnedValue==1){
-				result=true;
+			if (returnedValue == 1) {
+				result = true;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if(conn!=null){
+		} finally {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 		System.out.println(result);
 		return result;
 	}
-	
+
 }
