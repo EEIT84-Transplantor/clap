6776791efe0224df85077846cb23108d1dbb3914
caca <jlin218@uw.edu>
2016-03-30 18:02:39 +0800

    add MemberDAO, JDBCDAO, VO, Service

---
+++ Clap/src/main/java/service/MemberService.java
@@ -0,0 +1,26 @@
+package service;
+
+public class MemberService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+//	
+//	public memberVO login (String email, String password){
+//		
+//	}
+//	public memberVO signUp (String email, String password) {
+//		
+//	}
+//	public boolean changePassword (String email, String password) {
+//		
+//	}
+//	public boolean sendComfirmEmail (String email){
+//		
+//	}
+//	public memberVO findByEmail (Stirng email){
+//		
+//	}
+
+}
