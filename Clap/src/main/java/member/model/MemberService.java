acaa5b8190a266b6d38a8ad86ea6f1ef4248a058
EEIT84-Transplantor <jlin218218@gmail.com>
2016-03-30 21:29:14 +0800

    LoginServlet EmailExistServlet

--- Clap/src/main/java/member/model/MemberService.java
+++ Clap/src/main/java/member/model/MemberService.java
@@ -6,41 +6,44 @@ import member.model.MemberVO;
 
 public class MemberService {
 	private MemberDAO dao;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MemberService ms= new MemberService();
+		MemberService ms = new MemberService();
 		System.out.println(ms.login("andrew@gmail.com", "andrew"));
 	}
-	
-	public MemberService(){
+
+	public MemberService() {
 		dao = new MemberJdbcDAO();
 	}
-	
-	public MemberVO login (String email, String password){
-		MemberVO result=null;
+
+	public MemberVO login(String email, String password) {
+		MemberVO result = null;
 		MemberVO member = dao.selectByEmail(email);
-		if (member!= null){
+		if (member != null) {
 			byte[] temp = password.getBytes();
 			byte[] memberPassword = member.getPassword();
-			if (Arrays.equals(temp, memberPassword)){
+			if (Arrays.equals(temp, memberPassword)) {
 				result = member;
 			}
 		}
 		return result;
 	}
-//	public MemberVO signUp (String email, String password) {
-//		
-//	}
+	
+	// public MemberVO signUp (String email, String password) {
+	//
+	// }
 
-	public boolean changePassword (String email, String password) {
+	public boolean changePassword(String email, String password) {
 		return dao.update(email, password.getBytes());
 	}
 
-//	public boolean sendComfirmEmail (String email){
-//		
-//	}
+	 public boolean sendComfirmEmail (String email){
+		 
+		 return false;
+	 }
 
-	public MemberVO findByEmail (String email){
+	public MemberVO findByEmail(String email) {
 		MemberVO member = dao.selectByEmail(email);
 		return member;
 	}
