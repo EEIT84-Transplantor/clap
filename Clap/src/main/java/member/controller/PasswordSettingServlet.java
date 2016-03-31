726a2ec6eaf0f7e10df901861eec2f6c74a92ba8
caca <jlin218@uw.edu>
2016-03-31 11:04:34 +0800

    commit passwordservlet

--- Clap/src/main/java/member/controller/PasswordSettingServlet.java
+++ Clap/src/main/java/member/controller/PasswordSettingServlet.java
@@ -1,5 +1,18 @@
 package member.controller;
 
-public class PasswordSettingServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PasswordSettingServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response){
+		doPost(request, response);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
 
 }
