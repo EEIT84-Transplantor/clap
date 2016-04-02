2f13a7b61e9b6e6cd69fa84518adb188b775fbac
yali <yali@yali-PC>
2016-04-02 22:52:00 +0800

    U payment package

--- Clap/src/main/java/member/model/MemberVO.java
+++ Clap/src/main/java/member/model/MemberVO.java
@@ -3,6 +3,7 @@ package member.model;
 public class MemberVO {
 	private String email;
 	private byte[] password;
+	private Double gc_amount;
 
 	public String getEmail() {
 		return email;
@@ -16,4 +17,11 @@ public class MemberVO {
 	public void setPassword(byte[] password) {
 		this.password = password;
 	}
+	public Double getGc_amount() {
+		return gc_amount;
+	}
+	public void setGc_amount(Double gc_amount) {
+		this.gc_amount = gc_amount;
+	}
+	
 }
--- Clap/src/main/java/payment/model/CreditCardService.java
+++ Clap/src/main/java/payment/model/CreditCardService.java
@@ -1,8 +1,25 @@
 package payment.model;
 
+import java.util.Calendar;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class CreditCardService {
+	
+	public static void main(String[] args){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CreditCardService cservice = (CreditCardService)context.getBean("creditCardService");
+		CreditCardDAO cdao = (CreditCardDAO)context.getBean("creditCardDAO");
+		
+		CreditCardVO cvo = cdao.selectByCcNumber("1111222233334444");
+		System.out.println(cservice.isAvailable(cvo));
+		
+	}
+	
+	
+	
 	private CreditCardDAO dao;
 	
 	public CreditCardService() {
@@ -13,20 +30,107 @@ public class CreditCardService {
 	}
 	
 	public List<CreditCardVO> getCards(String email){		
-		return dao.selectByEmail(email);
-	};
+		List<CreditCardVO> result = null;
+		if(email!=null&&email.length()!=0){
+			List<CreditCardVO> temp = dao.selectByEmail(email);
+			if(temp!=null&&temp.size()!=0){
+				result = temp;
+			}
+		}
+		return result;
+	}
 	
 	public CreditCardVO setCard (CreditCardVO creditCardVO){
 		CreditCardVO result = null;
+		if(creditCardVO!=null){
+			String cc_cvv = creditCardVO.getCc_cvv();
+			String cc_goodrhru = creditCardVO.getCc_goodrhru();
+			String cc_number = creditCardVO.getCc_number();
+			String mb_email = creditCardVO.getMb_email();
+			
+			if(cc_cvv==null||cc_cvv.length()==0){
+				return result;
+			}
+			if(cc_goodrhru==null||cc_goodrhru.length()==0){
+				return result;
+			}
+			if(cc_number==null||cc_number.length()==0){
+				return result;
+			}
+			if(mb_email==null||mb_email.length()==0){
+				return result;
+			}
+			CreditCardVO temp = dao.insert(creditCardVO);
+			if(temp!=null){
+				result = temp;
+			}
+		}
 		return result;
-	};
+	}
 	
 	public boolean removeCard(String cc_number){
-		return dao.delete(cc_number);
-	};
+		boolean result = false;
+		if(cc_number!=null&&cc_number.length()!=0){
+			if(dao.delete(cc_number)){
+				result = true;
+			}
+		}
+		return result;
+	}
 	
 	public boolean isAvailable (CreditCardVO creditCardVO){
-		return false;
-	};
+		boolean result = false;
+		if(creditCardVO!=null){
+			String cc_number = creditCardVO.getCc_number();
+			if(cc_number!=null&&cc_number.length()!=0){
+				CreditCardVO temp = dao.selectByCcNumber(cc_number);
+				Calendar cal = null;
+				if(temp!=null){
+					//舊卡
+					//expire
+					int month = Integer.parseInt(temp.getCc_goodrhru().split("/")[0]);
+					int year = Integer.parseInt(temp.getCc_goodrhru().split("/")[1]);
+					cal = Calendar.getInstance();
+					int now_year =Integer.parseInt(String.valueOf(cal.get(Calendar.YEAR)).substring(2));
+					if(year>now_year){
+						return true;
+				    }
+					
+					int now_month =Integer.parseInt(String.valueOf(cal.get(Calendar.MONTH)))+1;
+					if(month>=now_month){
+						return true;
+				    }
+					
+				}else{
+					//新卡
+					
+					//卡號
+					if(temp.getCc_number()==null||temp.getCc_number().length()!=16){
+						return result;
+					}
+					
+					//cvv
+					if(temp.getCc_cvv()==null||temp.getCc_cvv().length()!=3){
+						return result;
+					}
+					
+					//expire
+					int month = Integer.parseInt(temp.getCc_goodrhru().split("/")[0]);
+					int year = Integer.parseInt(temp.getCc_goodrhru().split("/")[1]);
+					cal = Calendar.getInstance();
+					int now_year =Integer.parseInt(String.valueOf(cal.get(Calendar.YEAR)).substring(2));
+					if(year>now_year){
+						return true;
+				    }
+					
+					int now_month =Integer.parseInt(String.valueOf(cal.get(Calendar.MONTH)))+1;
+					if(month>=now_month){
+						return true;
+				    }
+				}
+			}
+		}
+		return result;
+	}
 
 }
