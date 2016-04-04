bd3f6c550d3f93cd631cb97767c9a47ec01df701
au4au4g4 <au4au4g4@gmail.com>
2016-04-05 00:30:58 +0800

    nothing

--- Clap/src/main/java/member/model/MemberDAOHibernate.java
+++ Clap/src/main/java/member/model/MemberDAOHibernate.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MemberDAOHibernate implements MemberDAO {
-
+	
 	private SessionFactory sessionFactory;
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
