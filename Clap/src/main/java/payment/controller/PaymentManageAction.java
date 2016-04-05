70d2a50c12a12f1d9b46c75020b79574cb4748b7
jlin218 <jlin218@uw.edu>
2016-04-05 21:17:46 +0800

    U paymentManageAction

--- Clap/src/main/java/payment/controller/PaymentManageAction.java
+++ Clap/src/main/java/payment/controller/PaymentManageAction.java
@@ -12,15 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.ServletActionContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpRequest;
 import org.springframework.transaction.support.ResourceTransactionManager;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ValidationAware;
 
+import member.model.MemberService;
+import payment.model.CreditCardDAO;
+import payment.model.CreditCardService;
 import payment.model.CreditCardVO;
 import payment.model.GiftCardService;
 import payment.model.GiftCardVO;
+import payment.model.PromoCodeService;
 import payment.model.PromoCodeVO;
 import payment.model.PromoService;
 
@@ -72,8 +78,8 @@ public class PaymentManageAction extends ActionSupport implements ValidationAwar
 	    		 addFieldError("error.errorMsg", "CVV is not valid");
 	    	 }
 	    	 String goodThru = creditCardVO.getCc_goodthru();
-	    	 if(goodThru==null||!Pattern.matches("^\\d{2}[/]\\d{2}$", goodThru)||Integer.parseInt(goodThru.substring(3))<Calendar.getInstance().YEAR||
-	    			 Integer.parseInt(goodThru.substring(3))<Calendar.getInstance().YEAR &&Integer.parseInt(goodThru.substring(0,2))<Calendar.getInstance().MONTH){
+	    	 if(goodThru==null||!Pattern.matches("^(0[1-9]|1[0-2])\\/?([0-9]{4}|[0-9]{2})$", goodThru)||Integer.parseInt(goodThru.substring(3))<Calendar.getInstance().get(Calendar.YEAR)||
+	    			 Integer.parseInt(goodThru.substring(3))==Calendar.getInstance().get(Calendar.YEAR) &&Integer.parseInt(goodThru.substring(0,2))<Calendar.getInstance().get(Calendar.MONTH)){
 	    		 addFieldError("error.errorMsg", "GoodThru is not valid");
 	    	 }
 	     }else if(buttonClicked.equalsIgnoreCase("Add Promo Code")){
@@ -112,14 +118,32 @@ public class PaymentManageAction extends ActionSupport implements ValidationAwar
 		HttpServletRequest request = ServletActionContext.getRequest();
 		RequestDispatcher rd= request.getRequestDispatcher("/payment/manage.controller");
 		HttpServletResponse response = ServletActionContext.getResponse();
-//		request.setAttribute("buttonClicked", creditCardVO);
-//		if (addType.equalsIgnoreCase("creditCard")){
-//			request.setAttribute("creditCardVO", creditCardVO);
-//		
-//		}else if (addType.equalsIgnoreCase("giftCard")){
-//			request.setAttribute("giftCard", "giftCard");
-//		}
-//		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		String email = (String) request.getSession().getAttribute("mb_email");
+		
+		if(buttonClicked.equalsIgnoreCase("Add Credit Card")){
+			CreditCardService cservice = (CreditCardService)context.getBean("creditCardService");
+			creditCardVO.setMb_email(email);
+			cservice.setCard(creditCardVO);			
+			
+		}else if(buttonClicked.equalsIgnoreCase("Add Gift Card")){
+			GiftCardService gservice = (GiftCardService)context.getBean("giftCardService");
+			Double amount = gservice.useCard(giftCardVO.getGc_number(), giftCardVO.getGc_code());
+			MemberService mservice = (MemberService)context.getBean("memeberService");
+			//mservice.addGiftCardAmount(amount);	
+		}else if(buttonClicked.equalsIgnoreCase("Add Promo Code")){
+			PromoService promoService = (PromoService)context.getBean("promoService");
+			if(promoService.isAvailable(promoCodeVO.getPc_code())){
+				PromoCodeService promoCodeService = (PromoCodeService)context.getBean("promoCodeService");
+				promoCodeService.setPromotionCode(email,promoCodeVO.getPc_code());
+			}
+			
+		}
+		
+		
+		
+		
 		try {
 			rd.forward(request, response);
 		} catch (ServletException e) {
