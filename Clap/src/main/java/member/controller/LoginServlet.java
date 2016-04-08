5b0cb65da7e0d0bfa46aae7e8cd6f31a687b8fcd
caca <jlin218@uw.edu>
2016-04-08 14:01:09 +0800

    U loginservlet

--- Clap/src/main/java/member/controller/LoginServlet.java
+++ Clap/src/main/java/member/controller/LoginServlet.java
@@ -1,9 +1,11 @@
 package member.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -74,14 +76,29 @@ public class LoginServlet extends HttpServlet {
 			String uri = (String) session.getAttribute("uri");
 			
 			List<CreditCardVO> payment = creditCardService.getCards(email);
- 			
+ 			List<String> cardType = new ArrayList<>();
+			for(CreditCardVO cardIt:payment){
+				String visa = "^4[0-9]{12}(?:[0-9]{3})?$";
+				String master = "^5[1-5][0-9]{14}$";
+				String jcb="^(?:2131|1800|35\\d{3})\\d{11}$";
+				String cardNum =cardIt.getCreditCard().getCc_number();
+				if(Pattern.matches(visa,cardNum)){
+					 cardType.add("visa");
+			    }else if(Pattern.matches(master,cardNum)){
+			    	cardType.add("master");
+			    }else if(Pattern.matches(jcb,cardNum)){
+			    	cardType.add("JCB");
+			    } else{
+			    	cardType.add("Master");	
+			    }	
+ 			}
  			Double amount = memberVO.getAmount();
  			
  			List<PromoVO>promoCodes=promoCodeService.getPromos(email);
  			session.setAttribute("cards",payment);
  			session.setAttribute("amount", amount);
  			session.setAttribute("promos", promoCodes);
-			
+ 			session.setAttribute("cardTypr", cardType);
 			
 			if(uri==null){
 				response.sendRedirect("../index.jsp");	
