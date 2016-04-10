4973c6c17a7c2d1a3d28b7dd734ad33deaff927a
au4au4g4 <au4au4g4@gmail.com>
2016-04-10 20:43:15 +0800

    U

--- Clap/src/test/java/shopping/model/TestOrderFormHibernateDAO.java
+++ Clap/src/test/java/shopping/model/TestOrderFormHibernateDAO.java
@@ -2,19 +2,32 @@ package shopping.model;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestOrderFormHibernateDAO {
 
-	private OrderFormHibernateDAO orderFormHibernateDAO;
-	private OrderFormVO orderFormVO;
 	private SessionFactory sessionFactory;
 	private Session session;
+	private OrderFormHibernateDAO orderFormHibernateDAO;
+	private Object expected;
+	private Object actual;
+	
+	private OrderFormVO orderFormVO;
+	private Integer id = 999;
+	private String mb_email = "999@gmail.com";
+	private Date time = new Date(new java.util.Date().getTime());
+	private Integer total = 99999;
+	private Byte os_id = 9;
 
 	@Before
 	public void setUp() {
@@ -22,35 +35,71 @@ public class TestOrderFormHibernateDAO {
 		sessionFactory = (SessionFactory) applicationContext.getBean("sessionFactory");
 		orderFormHibernateDAO = (OrderFormHibernateDAO) applicationContext.getBean("orderFormHibernateDAO");
 		orderFormVO = new OrderFormVO();
-		orderFormVO.setId(999);
 	}
 
 	@Test
-	public void testInsert() {
-		Integer expected = orderFormVO.getId();
+	public void testAInsert() {
 		session = sessionFactory.getCurrentSession();
 		session.beginTransaction();
-		System.out.println(orderFormHibernateDAO.insert(orderFormVO));
-		orderFormVO = orderFormHibernateDAO.select(orderFormVO.getId());
+
+		expected = id;
+		orderFormVO.setId(id);
+		orderFormHibernateDAO.insert(orderFormVO);
+		actual = orderFormHibernateDAO.select(id).getId();
+
 		session.getTransaction().commit();
-		Integer actual = orderFormVO.getId();
 		assertEquals(expected, actual);
 	};
 
 	@Test
-	public void testDelete() {
+	public void testBUpdate() {
+		session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+
+		expected = mb_email;
+		orderFormVO.setId(id);
+		orderFormVO.setMb_email(mb_email);
+		orderFormHibernateDAO.update(orderFormVO);
+		actual = orderFormHibernateDAO.select(id).getMb_email();
+
+		session.getTransaction().commit();
+		assertEquals(expected, actual);
 	};
 
 	@Test
-	public void testUpdate() {
+	public void testCSelect() {
+		session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+
+		expected = id;
+		actual = orderFormHibernateDAO.select(id).getId();
+		
+		session.getTransaction().commit();
+		assertEquals(expected, actual);
 	};
 
 	@Test
-	public void testSelect() {
+	public void testDDelete() {
+		session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+
+		Boolean expected = true;
+		Boolean actual = orderFormHibernateDAO.delete(id);
+
+		session.getTransaction().commit();
+		assertEquals(expected, actual);
 	};
 
 	@Test
 	public void testSelect2() {
+		session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+
+		expected=true;
+		actual=orderFormHibernateDAO.select().size()>10;
+		
+		session.getTransaction().commit();
+		assertEquals(expected, actual);
 	};
 
 }
