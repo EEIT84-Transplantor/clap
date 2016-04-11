149ca898842465b972c9670fc382da952ceb7f30
au4au4g4 <au4au4g4@gmail.com>
2016-04-11 21:35:49 +0800

    U

--- Clap/src/main/java/shopping/model/ReturnService.java
+++ Clap/src/main/java/shopping/model/ReturnService.java
@@ -1,5 +1,7 @@
 package shopping.model;
 
+import org.hibernate.SessionFactory;
+
 import inventory.model.InOutLogService;
 import product.model.ProductService;
 
@@ -8,6 +10,47 @@ public class ReturnService {
 	private ProductService productService;
 	private OrderDetailService orderDetailService;
 	private InOutLogService inOutLogService;
+	private SessionFactory sessionFactory;
+
+	public OrderFormService getOrderFormService() {
+		return orderFormService;
+	}
+
+	public void setOrderFormService(OrderFormService orderFormService) {
+		this.orderFormService = orderFormService;
+	}
+
+	public ProductService getProductService() {
+		return productService;
+	}
+
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
+	}
+
+	public OrderDetailService getOrderDetailService() {
+		return orderDetailService;
+	}
+
+	public void setOrderDetailService(OrderDetailService orderDetailService) {
+		this.orderDetailService = orderDetailService;
+	}
+
+	public InOutLogService getInOutLogService() {
+		return inOutLogService;
+	}
+
+	public void setInOutLogService(InOutLogService inOutLogService) {
+		this.inOutLogService = inOutLogService;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 	
 	
 }
