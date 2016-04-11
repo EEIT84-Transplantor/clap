17c30d57579634a1153e0b660f089c7565548566
EEIT84-Transplantor <jlin218218@gmail.com>
2016-04-11 10:16:58 +0800

    U

--- Clap/src/main/java/payment/model/CreditCardDAO.java
+++ Clap/src/main/java/payment/model/CreditCardDAO.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 public interface CreditCardDAO {
 	
-	public abstract List<CreditCardVO> selectByEmail(String mb_email);
+	public List<CreditCardVO> selectByEmail(String mb_email);
 	
-	public abstract CreditCardVO selectByCcNumber(String mb_email, String cc_number);
+	public CreditCardVO selectByCcNumber(String mb_email, String cc_number);
 
-	public abstract CreditCardVO insert(CreditCardVO creditCardVO);
+	public CreditCardVO insert(CreditCardVO creditCardVO);
 
-	public abstract boolean delete(String cc_number,String mb_email);
+	public boolean delete(String cc_number,String mb_email);
 
-	boolean delete(String cc_number);
 }
