eacc0fd13aea56fdc5987253c642ee94b5a46e02
yali <Student@III-EEIT>
2016-04-14 12:04:12 +0800

    D product.jsp I productDetail.jsp U ProductDetailAction.java

--- Clap/src/main/java/shopping/controller/ProductDetailAction.java
+++ Clap/src/main/java/shopping/controller/ProductDetailAction.java
@@ -1,5 +1,22 @@
 package shopping.controller;
 
-public class ProductDetailAction {
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProductDetailAction extends ActionSupport implements ServletRequestAware {
+    private HttpServletRequest request;
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+		
+	}
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+	
 
 }
