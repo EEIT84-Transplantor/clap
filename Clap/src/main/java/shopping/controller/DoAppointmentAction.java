4e48766d4d5266e1d9e2ed1a8eaafdd5470e84d8
EEIT84-Transplantor <jlin218218@gmail.com>
2016-04-18 21:25:49 +0800

    U

--- Clap/src/main/java/shopping/controller/DoAppointmentAction.java
+++ Clap/src/main/java/shopping/controller/DoAppointmentAction.java
@@ -1,5 +1,38 @@
 package shopping.controller;
 
-public class DoAppointmentAction {
+import com.opensymphony.xwork2.ActionSupport;
+
+import shopping.model.OrderFormVO;
+
+public class DoAppointmentAction extends ActionSupport {
+
+	private String orderList;
+	private String hospital;
+
+	public String getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(String orderList) {
+		this.orderList = orderList;
+	}
+
+
+
+
+	public String getHospital() {
+		return hospital;
+	}
+
+	public void setHospital(String hospital) {
+		this.hospital = hospital;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		System.out.println(orderList);
+		System.out.println(hospital);
+		return super.execute();
+	}
 
 }
