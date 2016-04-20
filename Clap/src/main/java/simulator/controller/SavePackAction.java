0536aaeb7b3365d12f7684ad21793c532e063f10
caca <jlin218@uw.edu>
2016-04-20 10:10:13 +0800

    C

--- Clap/src/main/java/simulator/controller/SavePackAction.java
+++ Clap/src/main/java/simulator/controller/SavePackAction.java
@@ -20,13 +20,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class SavePackAction extends ActionSupport{
 	private InputStream inputStream;
 	private List<Integer> productIds;
-	private Double weight;
-	private Double height;
-	private Integer smoking;
-	private Integer drinking;
-	private Integer exercising;
-	private Integer env_id;
-	
+
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
@@ -36,67 +30,23 @@ public class SavePackAction extends ActionSupport{
 	public void setProductIds(List<Integer> productIds) {
 		this.productIds = productIds;
 	}
-	public Double getWeight() {
-		return weight;
-	}
-	public void setWeight(Double weight) {
-		this.weight = weight;
-	}
-	public Double getHeight() {
-		return height;
-	}
-	public void setHeight(Double height) {
-		this.height = height;
-	}
-	public Integer getSmoking() {
-		return smoking;
-	}
-	public void setSmoking(Integer smoking) {
-		this.smoking = smoking;
-	}
-	public Integer getDrinking() {
-		return drinking;
-	}
-	public void setDrinking(Integer drinking) {
-		this.drinking = drinking;
-	}
-	public Integer getExercising() {
-		return exercising;
-	}
-	public void setExercising(Integer exercising) {
-		this.exercising = exercising;
-	}
-	public Integer getEnv_id() {
-		return env_id;
-	}
-	public void setEnv_id(Integer env_id) {
-		this.env_id = env_id;
-	}
+
 	public String execute(){
 		HttpServletRequest request=ServletActionContext.getRequest();
 		HttpSession session = request.getSession();
 		JSONObject jsonObject = new JSONObject();
-		Map<String, Number> map = new HashMap<>();
-		map.put("height", height);
-		map.put("weight", weight);
-		map.put("smoking", smoking);
-		map.put("drinking", drinking);
-		map.put("exercising", exercising);
-		map.put("env_id", env_id);
+
 		String JSONstr="";
 		Boolean success=false;
 		if (session.getAttribute("savePack1")==null){
-			session.setAttribute("conditions1", map);
 			session.setAttribute("savePack1", productIds);
 			success=true;
 		}else if(session.getAttribute("savePack2")==null){
 			session.setAttribute("savePack2", productIds);
-			session.setAttribute("conditions2", map);
 			success=true;
 			jsonObject.put("savedIn", "two");
 		}else if(session.getAttribute("savePack3")==null){
 			session.setAttribute("savePack3", productIds);
-			session.setAttribute("conditions3", map);
 			success=true;
 			jsonObject.put("savedIn", "three");
 		}
--- Clap/src/main/java/simulator/model/SimulatorService.java
+++ Clap/src/main/java/simulator/model/SimulatorService.java
@@ -18,7 +18,18 @@ public class SimulatorService {
 		//if bmi ==null, ignore this factor
 		//smoking, drinking and exercising will be from 0-100
 		//env_id will be null if nothing is passed in
-		
+		SimulatorVO simulatorVO = new SimulatorVO();
+		if (env_id!=null){
+			
+		}else if(bmi!=null){
+			
+		}else if(smoking!=50){
+			
+		}else if(drinking!=50){
+			
+		}else if(exercising!=50){
+			
+		}
 		return null;
 	}
 
