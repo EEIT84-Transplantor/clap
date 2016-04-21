6b2d7756b70a9406eb128f7275b3c44f5826c4b7
EEIT84-Transplantor <jlin218218@gmail.com>
2016-04-21 11:08:38 +0800

    U

--- Clap/src/main/java/simulator/controller/OneclickPackAction.java
+++ Clap/src/main/java/simulator/controller/OneclickPackAction.java
@@ -5,45 +5,30 @@ import java.util.List;
 import org.apache.struts2.convention.annotation.InterceptorRef;
 import org.apache.struts2.convention.annotation.InterceptorRefs;
 import org.apache.struts2.json.annotations.JSON;
+import org.hibernate.property.access.spi.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import shopping.model.OrderDetailVO;
+
 public class OneclickPackAction extends ActionSupport {
 
-	private List<Report> data;
-	private int[] ints = { 10, 20 };
-
-	public String execute() {
-		try {
-
-			System.out.println(data.size());
-			for (int i = 0; i < data.size(); i++) {
-				System.out.println("Data  " + data.get(i).getColor() + "-" + data.get(i).getDate() + "-"
-						+ data.get(i).getId() + "-" + data.get(i).getName());
-			}
-			System.out.println("Execute Method");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return SUCCESS;
+	private List<OrderDetailVO> productList;
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
 	}
 
-	public List<Report> getData() {
-		System.out.println("Getter Call");
-		return data;
+	public List<OrderDetailVO> getProductList() {
+		return productList;
 	}
 
-	public void setData(List<Report> data) {
-		System.out.println("Setter Call Flow");
-		this.data = data;
-	}
-
-	public int[] getInts() {
-		return ints;
-	}
-
-	public void setInts(int[] ints) {
-		this.ints = ints;
+	public void setProductList(List<OrderDetailVO> productList) {
+		this.productList = productList;
 	}
 
 }
