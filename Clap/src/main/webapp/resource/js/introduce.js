2022069157603805d73529aa373cfe54586b6c97
au4au4g4 <au4au4g4@gmail.com>
2016-04-29 01:16:15 +0800

    U

--- Clap/src/main/webapp/resource/js/introduce.js
+++ Clap/src/main/webapp/resource/js/introduce.js
@@ -5,4 +5,10 @@ $(function() {
 	$(window).on("mousewheel", function(event) {
 		TweenMax.to(window, 1, { scrollTo : { y : "-=" + event.deltaY * h/5  }, ease : Power3.easeOut });
 	});
+	
+	$("path").each(function(){
+		var lineLength=this.getTotalLength();
+		$(this).css("stroke-dasharray", lineLength);
+		$(this).css("stroke-dashoffset", lineLength);
+	})
 });
