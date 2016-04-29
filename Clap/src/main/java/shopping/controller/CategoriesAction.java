0febb80e9a2762ce5aeffc73e705ea0bc1d39aa3
Student <Student@III-EEIT>
2016-04-29 12:16:19 +0800

    U

--- Clap/src/main/java/shopping/controller/CategoriesAction.java
+++ Clap/src/main/java/shopping/controller/CategoriesAction.java
@@ -4,13 +4,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Index;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 import product.model.CategoryService;
+import product.model.CategoryVO;
 import product.model.ProductService;
 import product.model.ProductVO;
 import product.model.ProductimgVO;
@@ -38,14 +42,33 @@ public class CategoriesAction extends ActionSupport implements ServletRequestAwa
 
 	@Override
 	public String execute() throws Exception {
+		
+		ServletContext context = ServletActionContext.getServletContext();
+		List<CategoryVO> globalCategoryVOs = (List<CategoryVO>)context.getAttribute("globalCtegoryVOs");
+		Map<Integer,List<ProductVO>> globalProductVOs = (Map<Integer,List<ProductVO>>)context.getAttribute("globalProductVOs");
+		Map<Integer,List<ProductimgVO>> globalProductimgVOs = (Map<Integer,List<ProductimgVO>>)context.getAttribute("globalProductimgVOs");
+		
+		
+		CategoryVO target = null;
 		if(organ==null){
-			organ = categoryService.getAllCategory().get(0).getName();
+			organ = globalCategoryVOs.get(0).getName();
 		}
-		List<ProductVO> lists = productService.searchProductByCategory(categoryService.selectByCategoryName(organ));
+		int index = 0;
+		for(CategoryVO categoryVO:globalCategoryVOs){
+			if(categoryVO.getName().equals(organ)){
+				target = categoryVO;
+				break;
+			}
+			index++;
+		}
+		
+		List<ProductVO> lists = globalProductVOs.get(target.getId());
+		List<ProductimgVO> imglists =  globalProductimgVOs.get(target.getId());
 		Map<Integer, ProductimgVO> productimg = new HashMap<Integer, ProductimgVO>();
 		
 		for(ProductVO vo:lists){
-			productimg.put(vo.getId(), productService.getProductImgById(vo.getId()));
+			               //產品id       產品圖片
+			productimg.put(vo.getId(), imglists.get(index));
 		}
 		
 		
