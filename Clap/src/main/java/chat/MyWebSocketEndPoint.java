431555bd5d542cf20cf860939fe7324058b92242
jlin218 <jlin218@uw.edu>
2016-05-01 12:59:41 +0800

    C

--- Clap/src/main/java/chat/MyWebSocketEndPoint.java
+++ Clap/src/main/java/chat/MyWebSocketEndPoint.java
@@ -11,7 +11,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/chat")
+@ServerEndpoint(value = "/chat.c")
 public class MyWebSocketEndPoint {
 //
 private static final String USERNAME_KEY = "username";
